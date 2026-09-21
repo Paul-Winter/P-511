c841c8c1571967a4ea274ca138e1e00332ddbf75
Бочаров <bocharowaolga45@gmail.com>
2026-09-22 01:05:58 +0300

    Добавлен полный проект Колендарь.

---
+++ Курсовой проект Desktop приложения/Бочаров/DailyPlanner/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace DailyPlanner
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        private void InitializeComponent()
+        {
+            this.calendar = new System.Windows.Forms.MonthCalendar();
+            this.gridTasks = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnReschedule = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gridTasks)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // calendar
+            // 
+            this.calendar.BackColor = System.Drawing.Color.DimGray;
+            this.calendar.CalendarDimensions = new System.Drawing.Size(5, 2);
+            this.calendar.ForeColor = System.Drawing.Color.DimGray;
+            this.calendar.Location = new System.Drawing.Point(18, 18);
+            this.calendar.Name = "calendar";
+            this.calendar.TabIndex = 0;
+            this.calendar.Tag = "calendar";
+            this.calendar.TitleBackColor = System.Drawing.Color.DimGray;
+            this.calendar.TitleForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.calendar.UseWaitCursor = true;
+            this.calendar.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.calendar_DateChanged);
+            // 
+            // gridTasks
+            // 
+            this.gridTasks.AllowUserToAddRows = false;
+            this.gridTasks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridTasks.Location = new System.Drawing.Point(916, 18);
+            this.gridTasks.Name = "gridTasks";
+            this.gridTasks.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.gridTasks.Size = new System.Drawing.Size(476, 311);
+            this.gridTasks.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(978, 335);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Добавить";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(1059, 335);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 3;
+            this.btnEdit.Text = "Изменить";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(1221, 335);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Удалить";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnReschedule
+            // 
+            this.btnReschedule.Location = new System.Drawing.Point(1140, 335);
+            this.btnReschedule.Name = "btnReschedule";
+            this.btnReschedule.Size = new System.Drawing.Size(75, 23);
+            this.btnReschedule.TabIndex = 5;
+            this.btnReschedule.Text = "Перенести";
+            this.btnReschedule.UseVisualStyleBackColor = true;
+            this.btnReschedule.Click += new System.EventHandler(this.btnReschedule_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(1404, 612);
+            this.Controls.Add(this.btnReschedule);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.gridTasks);
+            this.Controls.Add(this.calendar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.gridTasks)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private System.Windows.Forms.MonthCalendar calendar;
+        private System.Windows.Forms.DataGridView gridTasks;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnReschedule;
+    }
+}
+
