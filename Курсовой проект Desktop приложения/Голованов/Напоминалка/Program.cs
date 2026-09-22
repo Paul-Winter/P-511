a8ff8d18f8bbf8e2fbed7dc5d179be3599c71d06
Paul Winter <zimin.pavel.v@gmail.com>
2026-09-22 19:05:13 +0300

    КР

---
+++ Курсовой проект Desktop приложения/Голованов/Напоминалка/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Xml.Serialization;
+
+namespace Напоминалка
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello. What is it that you want to remind yourself of?");
+        }
+        Import? time;
+
+        Import sys;
+{
+{
+
+
+
+                Def Set_reminder():
+        prite(-----Программа Напоминания--
+
+        try:
+
+            second = int(input("Введит ко скольки 
+
+            message = input("Введите текст
+
+            except ValueError:
+
+            print("Ошибка: нужно ввести цель
+            return
+
+            Print(f"Напоминания установлено")
+
+            # Обратный  отсеч или ожидание 
+
+                }
